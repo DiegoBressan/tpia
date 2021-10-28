11b16f9d414f1b1b9e252a6bb484b88569b0b961
DiegoBressan <diegocarpbressan@gmail.com>
2021-10-28 10:02:56 -0300

    .

---
+++ TP1/tp1.py
@@ -0,0 +1,30 @@
+from simpleai.search import (
+    SearchProblem,
+    astar,
+    breadth_first,
+    depth_first,
+    uniform_cost,
+    limited_depth_first,
+    iterative_limited_depth_first,
+)
+from simpleai.search.viewers import WebViewer
+
+class problem1(SearchProblem):
+
+    def is_goal(self, state):
+        return 
+    
+    def capacidad(self, state):
+        return 
+    
+    def actions(self, state):
+        return 
+    
+    def cost(self, state, action, state2):
+        return 
+    
+    def result(self, state, action):
+        return 
+    
+    def heuristic(self, state):
+        return 
