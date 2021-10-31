036b0b2f944b87ca44c19a8c0931fca45c74db0a
DiegoBressan <diegocarpbressan@gmail.com>
2021-10-31 12:58:42 -0300

    entrega 1 casi terminada, falta actions y realizar los test

--- entrega1.py
+++ entrega1.py
@@ -1,26 +1,62 @@
 from simpleai.search import (
     SearchProblem,
     astar,
-)
+) 
 
-def is_goal(self, state):
-    # Todas las zonas tienen que ser escaneadas en el menor tiempo posible
-    return 
-    
-def actions(self, state):
-    return 
-    
-def cost(self, state, action, state2):
-    # Moverse de zona = 1 minuto (100 mAh)
-    # Recargar la energia de un robot mapeador = 5 minutos
-    return 
-    
-def result(self, state, action):
-    return 
-    
-def heuristic(self, state):
-    # Cantidad de zonas que faltan esplorar
-    return 
+def planear_escaneo(tuneles,robots):
 
-def planear_escaneo(self,state):
+    class exploracionrobotica(SearchProblem):
+
+        def is_goal(self, state):
+            # Todas las zonas tienen que ser escaneadas en el menor tiempo posible
+            tunel, robot = state
+            aux = 0
+
+            for x in tunel:
+                if x == 'E':
+                    aux +=1
+            
+            if aux == len(tunel):
+                return True
+            else:
+                return False   
+            
+        def actions(self, state):
+            # Moverse de zona
+
+            # Recargar energia
+
+            action = (zonaaction, robotaction)
+            return action
+                
+        def cost(self, state, action):
+            if action == 'moverzona': # Moverse de zona = 1 minuto (100 mAh)
+                return 1
+            elif action == 'recargarrobot': # Recargar la energia de un robot mapeador = 5 minutos
+                return 5
+                
+        def result(self, state, action):
+            zonaaction, robotaction = action
+            tunel, robot = state
+            tuplatunel = list(tunel)
+            tuplarobot = list(robot)
+            tupla = []
+
+            if action == 'recargarrobot': # Resultado de recargar robot
+                for x in tuplarobot:
+                    if x == robotaction:
+                        x = 1000
+            elif action == 'moverzona': # Resultado de mover robot de zona      
+                for y in tuplatunel:
+                    if y == zonaaction:
+                        y = 'E'
+
+            tupla = (tuplatunel, tuplarobot)
+            return tupla
+                
+        def heuristic(self, state):
+            # Cantidad de zonas que faltan esplorar
+            tunel, robot = state
+            return len([x for x in tunel if x != 'E'])
+
     return
--- entrega2.py
+++ entrega2.py
@@ -3,20 +3,6 @@ from simpleai.search import (
     astar,
 )
 
-def is_goal(self, state):
-    return 
-    
-def actions(self, state):
-    return 
-    
-def cost(self, state, action, state2):
-    return 
-    
-def result(self, state, action):
-    return 
-    
-def heuristic(self, state):
-    return 
-
 def rediseñar_robot(self,state):
+    
     return
