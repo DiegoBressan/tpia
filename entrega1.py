faf1b16a247b64409facd42305e17523357d65db
Maximiliano Mainero <maxx321xx@gmail.com>
2021-11-04 15:33:45 -0300

    attempt 1

--- entrega1.py
+++ entrega1.py
@@ -1,119 +1,108 @@
 from simpleai.search import (
     SearchProblem,
     astar,
-) 
-'''
-def convertirlista(tupla): # Se pasa de tupla a lista
-    convertir = list()
-    for x in tupla:
-        convertir.append(x)
-    return convertir
+)
 
-def convertirtupla(lista): # Se pasa de lista a tupla
-    convertir = tuple(lista)
-    return convertir
-'''
-        
-def planear_escaneo(tuneles,robots):
+def planear_escaneo(tuneles, robots):
 
-    inicio = (tuneles,[])
+    # Setup values
+    # Se reliza una lista con 2 posiciones donde almacenamos:
+    # [0] El estado los túneles que están pendientes de revision
+    # [1] Una lista de robots con:
+        # [1][0] El tipo de robot,
+        # [1][1] La posicion inicial 5,1 ya que la 5,0 no debemos evaluarla
+        # [1][2] Se le asigna una carga (default 1000 para los escaneadores, 9999 para los soportes)
 
+    INITIAL_STATE = (tuneles, [])
     for robot in robots:
-        if(robot[1] == "escaneador"):
-            inicio[1].append((robot[0], (5,0), 1000))
+        if robot[0][0] == 'e':            
+            INITIAL_STATE[1].append((robot[0], (5, 0), 1000))
         else:
-            inicio[1].append((robot[0], (5,0), 5000))
-    
-    inicio = tuple(inicio)
+            INITIAL_STATE[1].append((robot[0], (5, 0), 9999))
 
-    class exploracionrobotica(SearchProblem):
+    INITIAL_STATE = tuple([tuple(x) for x in INITIAL_STATE])
 
+    class Problema(SearchProblem):
         def is_goal(self, state):
-            # No tiene que tener zonas pendientes por escanear
-            if len(state) == 0:
+            tuneles_pendientes = state[0]
+            if len(tuneles_pendientes) == 0:
                 return True
-            else:
-                return False
-                    
-        def cost(self, state, action):
-            x, y, accion = action
-
-            if accion == 'moverzona': # Moverse de zona = 1 minuto (100 mAh)
-                return 1
-            else: # Recargar la energia de un robot mapeador = 5 minutos
-                return 5   
+            return False
         
         def actions(self, state):
-            tunel, robot = state
-            accion = []
-
-            for rob in robot:
-                id, zona, bateria = rob
-                
-                if bateria == 5000: # Recargar energia
-                    for robcarga in robot:
-                        id2, zona2, bateria2 = robcarga
-
-                        if (zona2 == zona) and (bateria2 < 1000):
-                            accion.append((zona, id2, 'recargarrobot'))
-                
-                if bateria >= 100: # Moverse de zona
-                    posiciones_posibles = [(zona[0], zona[1] - 1), (zona[0] - 1, zona[1]), (zona[0], zona[1] + 1), (zona[0] + 1, zona[1])]
-
-                    for posicion in posiciones_posibles:
-                        if posicion in tunel:
-                            accion.append((posicion, id, 'moverzona'))
-
-            return tuple(accion)
-                    
-        def result(self, state, action):
-            zonaaction, robotaction, accion = action
-            tuneles, robots = state
-            listatunel = list(tuneles)
-            listarobot = list(robots)
-
-            if accion == 'recargarrobot': # Resultado de recargar robot
-                listarobotcargar = list()
-                for rob in enumerate(robots):
-                    if rob[1][2] == zonaaction:
-                        listarobotcargar.append(rob)
-                        break
-                robotcargar = listarobotcargar[0]
-                id_robotcargar = robotcargar[0]
-                robots[id_robotcargar][2] = 1000
-            elif accion == 'moverzona': # Resultado de mover robot de zona  
-                listarobotmover = list()
-                for rob in enumerate(robots):
-                    if rob[0] == robotaction:
-                        listarobotmover.append(rob)
-                        break    
-                robotmover = listarobotmover[0]
-                id_robotmover = robotmover[0]
-                robots[id_robotmover][1] = zonaaction
-
-                if(robotmover[1][2] <= 1000): # Si es escaneador
-                    robots[id_robotmover][2] -= 100
-                    
-                    if zonaaction in listatunel: # Se elimina la zona escaneada
-                        listatunel.remove(zonaaction)
+            tuneles_pendientes, robots_attrs = state
+            movimientos_disponibles = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            acciones_disponibles = [] # Las acciones tendrán 3 campos: [0] robot [1] tipo de accion (escanear/cargar) [2] prox posicion o carga
             
-            listatunel = tuple(listatunel)
-            listarobot = tuple(listarobot)
+            for robot in robots_attrs:
+                if robot[0][0] == 'e' and robot[2] >= 100: # Es un robot scaneador y puede seguir escaneando ya que tiene >100 de bateria
+                    for movimiento in movimientos_disponibles:
+                        proxima_posicion = [list(robot[1])[0] + movimiento[0], list(robot[1])[1] + movimiento[1]]
+                        if tuple(proxima_posicion) in tuneles_pendientes:
+                            acciones_disponibles.append([robot[0], "mover", proxima_posicion])
+                elif robot[0][0] == 's':
+                    for robot_a_cargar in robots_attrs:
+                        if robot[1] == robot_a_cargar[1] and robot_a_cargar[2] < 1000 and robot_a_cargar[0][0] == 'e':
+                            acciones_disponibles.append([robot[0], "cargar", robot_a_cargar[0]])
 
-            return tuple(listatunel, listarobot)
-                    
+            return acciones_disponibles
+
+        
+        def cost(self, initial_state, action, end_state):
+            if action[1] == "mover":
+                return 1
+            return 5
+        
+        def result(self, state, action):
+            robot_accion, tipo_accion, posicion_o_robot = action
+            tuneles, robots = state
+
+            robots = [list(x) for x in robots]
+            tuneles = list(tuneles)
+
+            if tipo_accion == "mover":
+                robot = [x for x in robots if x[0] == robot_accion]
+                robot = robot[0]
+                posicion_o_robot = tuple(posicion_o_robot)
+                robot[1] = posicion_o_robot
+                robot[2] -= 100
+                if posicion_o_robot in tuneles:
+                    tuneles.remove(posicion_o_robot)
+            else:
+                robot_a_cargar = [x for x in robots if x[0] == posicion_o_robot]
+                robot_a_cargar = robot_a_cargar[0]
+                robot_a_cargar[2] = 1000
+            
+            robots = tuple([tuple(x) for x in robots])
+            tuneles = tuple(tuneles)
+
+            print((tuneles, robots))
+
+            return (tuneles, robots)
+
         def heuristic(self, state):
-            # Cantidad de zonas que faltan esplorar
-            tunel, robot = state
-            return len(tunel)
+            return len(state[0])
 
-    problema = exploracionrobotica(inicio)
+    problema = Problema(INITIAL_STATE)
+
     resultado = astar(problema, graph_search=True)
-
     plan = []
 
-    for x in resultado.path():
-        if x is not None:
-            plan.append(x)
+    for accion, state in resultado.path():
+        # Descartar la primera acción que es None
+        if (accion is not None):
+            plan.append(accion)
 
-    return tuple(plan)
+    return plan
+
+"""if __name__ == '__main__':    
+    
+    #test
+    tuneles = (
+        (3, 3),
+        (4, 3),
+        (5, 1), (5, 2), (5, 3),
+    )
+    robots = (("e1", "escaneador"), ("s1", "soporte"))
+
+    plan = planear_escaneo(tuneles, robots)"""
